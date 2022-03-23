dec5e01a25413a2146ea4eb9f89813335b2d77e6
SunnyMinh317 <vunhatminh317@gmail.com>
2022-03-23 20:51:31 +0700

    New commit

--- GolfGameLTNC/headers/Ball.h
+++ GolfGameLTNC/headers/Ball.h
@@ -1,1 +1,103 @@
 #pragma once
+//The Ball that will move around on the screen
+class Ball
+{
+public:
+	//The dimensions of the Ball
+	static const int Ball_WIDTH = 20;
+	static const int Ball_HEIGHT = 20;
+
+	//Maximum axis velocity of the Ball
+	static const int Ball_VEL = 640;
+
+	//Initializes the variables
+	Ball();
+
+	//Takes key presses and adjusts the Ball's velocity
+	void handleEvent(SDL_Event& e);
+
+	//Moves the Ball
+	void move(float timeStep);
+
+	//Shows the Ball on the screen
+	void render();
+
+private:
+	float mPosX, mPosY;
+	float mVelX, mVelY;
+};
+
+Ball::Ball()
+{
+	//Initialize the position
+	mPosX = 0;
+	mPosY = 0;
+
+	//Initialize the velocity
+	mVelX = 0;
+	mVelY = 0;
+}
+
+void Ball::handleEvent(SDL_Event& e)
+{
+	//If a key was pressed
+	if (e.type == SDL_KEYDOWN && e.key.repeat == 0)
+	{
+		//Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP: mVelY -= Ball_VEL; break;
+		case SDLK_DOWN: mVelY += Ball_VEL; break;
+		case SDLK_LEFT: mVelX -= Ball_VEL; break;
+		case SDLK_RIGHT: mVelX += Ball_VEL; break;
+		}
+	}
+	//If a key was released
+	else if (e.type == SDL_KEYUP && e.key.repeat == 0)
+	{
+		//Adjust the velocity
+		switch (e.key.keysym.sym)
+		{
+		case SDLK_UP: mVelY += Ball_VEL; break;
+		case SDLK_DOWN: mVelY -= Ball_VEL; break;
+		case SDLK_LEFT: mVelX += Ball_VEL; break;
+		case SDLK_RIGHT: mVelX -= Ball_VEL; break;
+		}
+	}
+}
+
+void Ball::move(float timeStep)
+{
+	//Move the Ball left or right
+	mPosX += mVelX * timeStep;
+
+	//If the Ball went too far to the left or right
+	if (mPosX < 0)
+	{
+		mPosX = 0;
+	}
+	else if (mPosX > SCREEN_WIDTH - Ball_WIDTH)
+	{
+		mPosX = SCREEN_WIDTH - Ball_WIDTH;
+	}
+
+	//Move the Ball up or down
+	mPosY += mVelY * timeStep;
+
+	//If the Ball went too far up or down
+	if (mPosY < 0)
+	{
+		mPosY = 0;
+	}
+	else if (mPosY > SCREEN_HEIGHT - Ball_HEIGHT)
+	{
+		mPosY = SCREEN_HEIGHT - Ball_HEIGHT;
+	}
+}
+
+void Ball::render()
+{
+	//Show the Ball
+	gBallTexture.render((int)mPosX, (int)mPosY);
+}
+
--- GolfGameLTNC/main.cpp
+++ GolfGameLTNC/main.cpp
@@ -1,7 +1,189 @@
-#include <iostream>
+/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
+and may not be redistributed without written permission.*/
+
+//Using SDL, SDL_image, standard IO, and, strings
 #include <SDL.h>
 #include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+//The window we'll be rendering to
+SDL_Window* gWindow = NULL;
+
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
+
+#include "headers/Texture.h"
 #include "headers/Ball.h"
-int main(int argc, char *args[]) {
-    
+#include "headers/Hole.h"
+#include "headers/Timer.h"
+
+//Starts up SDL and creates window
+bool init();
+
+//Loads media
+bool loadMedia();
+
+//Frees media and shuts down SDL
+void close();
+
+bool init()
+{
+	//Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not initialize! SDL Error: %s\n", SDL_GetError());
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer(gWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (gRenderer == NULL)
+			{
+				printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if (!(IMG_Init(imgFlags) & imgFlags))
+				{
+					printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	//Loading success flag
+	bool success = true;
+
+	//Load Ball texture
+	if (!gBallTexture.loadFromFile("pictures/dot.bmp"))
+	{
+		printf("Failed to load Ball texture!\n");
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	//Free loaded images
+	gBallTexture.free();
+
+	//Destroy window	
+	SDL_DestroyRenderer(gRenderer);
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int main(int argc, char* args[])
+{
+	//Start up SDL and create window
+	if (!init())
+	{
+		printf("Failed to initialize!\n");
+	}
+	else
+	{
+		//Load media
+		if (!loadMedia())
+		{
+			printf("Failed to load media!\n");
+		}
+		else
+		{
+			//Main loop flag
+			bool quit = false;
+
+			//Event handler
+			SDL_Event e;
+
+			//The Ball that will be moving around on the screen
+			Ball Ball;
+
+			//Keeps track of time between steps
+			LTimer stepTimer;
+
+			//While application is running
+			while (!quit)
+			{
+				//Handle events on queue
+				while (SDL_PollEvent(&e) != 0)
+				{
+					//User requests quit
+					if (e.type == SDL_QUIT)
+					{
+						quit = true;
+					}
+
+					//Handle input for the Ball
+					Ball.handleEvent(e);
+				}
+
+				//Calculate time step
+				float timeStep = stepTimer.getTicks() / 1000.f;
+
+				//Move for time step
+				Ball.move(timeStep);
+
+				//Restart step timer
+				stepTimer.start();
+
+				//Clear screen
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+				SDL_RenderClear(gRenderer);
+
+				//Render Ball
+				Ball.render();
+
+				//Update screen
+				SDL_RenderPresent(gRenderer);
+			}
+		}
+	}
+
+	//Free resources and close SDL
+	close();
+
+	return 0;
 }
